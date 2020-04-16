34b9cbb2c48abba916eee2be320020aea29a1229
ldaug99 <lassekd99@live.dk>
2020-04-16 20:32:24 +0200

    Made debug print value of HTML placeholder on HTML processor

--- src/WebAPI.cpp
+++ src/WebAPI.cpp
@@ -161,7 +161,25 @@ int16_t WebAPI::findPlaceholderIndex(const String &placeholder) {
 		if (placeholder.indexOf(_apiKeywords[i].htmlPlaceholder) >= 0) {
 			#ifdef useVerboseSerial
 				Serial.print("WebAPI::findPlaceholderIndex(), Found HTML placeholder with index: ");
-				Serial.println(i);
+				Serial.print(i);
+				Serial.print(" , and value: ");
+				switch (_apiKeywords[i].valueType) {
+					case FLOAT: {
+						Serial.println(String(_apiKeywords[i].keyValue.floatValue));
+					} break;
+					case INT: {
+						Serial.println(String(_apiKeywords[i].keyValue.intValue));
+					} break;
+					case UINT: {
+						Serial.println(String(_apiKeywords[i].keyValue.uintValue));
+					} break;
+					case CHARA: {
+						Serial.println(String(_apiKeywords[i].keyValue.charArrayValue));
+					} break;
+					case BOOL: {
+						Serial.println(String(_apiKeywords[i].keyValue.boolValue));
+					} break;
+				}
 			#endif
 			return i;
 		}
